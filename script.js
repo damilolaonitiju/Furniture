57872708de170f5d23b41b7ea5b2149851060d63
damilolaonitiju <83452297+damilolaonitiju@users.noreply.github.com>
2023-04-24 02:03:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//show and hide currency drop down
+
+const money = document.querySelector(".money");
+const monies = document.querySelector(".monies");
+
+monies.classList.add("hidden");
+
+money.addEventListener("click", function () {
+  monies.classList.toggle("hidden");
+});
+
 //populating the website with data from a fake api store
 
 fetch("https://fakestoreapi.com/products")
@@ -11,7 +22,6 @@ fetch("https://fakestoreapi.com/products")
     <h1 class="title">${values.title}</h1>
     <img src=${values.image}
     alt="img" class="images">
-    <p class='description'>${values.description}</p>
     <p class="category">${values.category}</p>
     <p class="price" style="font-size:1.5rem" data-price="${values.price}">${values.price}</p>
 
@@ -26,13 +36,12 @@ fetch("https://fakestoreapi.com/products")
 
 // Get exchange rate from API
 function getExchangeRate(targetCurrency) {
-  const url = `https://v6.exchangerate-api.com/v6/b9dd02de2570891bbf605d4a/latest/USD`;
+  // const url = `https://v6.exchangerate-api.com/v6/b9dd02de2570891bbf605d4a/latest/USD`;
   return fetch(url)
     .then((response) => response.json())
     .then((data) => data.conversion_rates[targetCurrency]);
 }
 
-// Update prices based on selected currency
 // Update prices based on selected currency
 function updatePrices(targetCurrency) {
   const prices = document.querySelectorAll(".price");
@@ -54,4 +63,12 @@ updatePrices("USD");
 const select = document.getElementById("currency-selector");
 select.addEventListener("change", (event) => {
   updatePrices(event.target.value);
+});
+
+//nav menu
+const hamburger = document.querySelector(".hamburger");
+const navLink = document.querySelector(".nav-link");
+
+hamburger.addEventListener("click", () => {
+  navLink.classList.toggle("show");
 });
